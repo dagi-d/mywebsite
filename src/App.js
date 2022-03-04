33163d8754edeb2b60451ccbb2c94bc640a62542
dagi-d <dagemawibekele@gmail.com>
2022-03-04 03:12:05 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from "react"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Testimonial from "./components/Skills"
 
-import './App.css';
-
-function App() {
+export default function App(){
   return (
-    <h1> hello dagi</h1>
+    <main className="text-gray-400 bg-gray-900 body-font">
+    <Navbar/>
+      <About/>
+      <Projects/>
+      <Testimonial/>
+      <Contact/>
+    </main>
   );
 }
-
-export default App;
